22760cc3453943de377e132fa2c64817899e2bd4
AKM_Machine <anindya15l12@gmail.com>
2021-05-03 23:42:01 +0530

    Realtime Button implemented, Macros used for pins

--- MicroServoBot.ino
+++ MicroServoBot.ino
@@ -1,58 +1,71 @@
-#include <Servo.h> 
-const int  buttonPin = 2;    
-const int ledPin = 13;      
+#include <Servo.h>
+
+#define buttonPin 2 //can only be D2 or D3 on Arduino Uno
+//check https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/ for other boards
+#define ledPin 13
+
+#define sensorPin0 A0    // Finger
+#define sensorPin1 A1    // Hand
+#define sensorPin2 A2    // Hinge   
 
 int buttonPushCounter = 1;   
-int buttonState = 0;         
-int lastButtonState = 0;     
+bool buttonState;  
 
 Servo servo_0;
 Servo servo_1;
 Servo servo_2;
 
-int sensorPin0 = A0;    // Finger
-int sensorPin1 = A1;    // Hand
-int sensorPin2 = A2;    // Hinge
 int SensVal[3];
 float state_0[3],state_1[3],state_2[3];
+
+void buttonTicker() {
+  buttonState = HIGH;
+}
+
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
+ 
   servo_0.attach(3);
   servo_1.attach(5);
   servo_2.attach(10);
+ 
   Serial.begin(9600);
-  Serial.println("mini robot ready...");  
+  Serial.println("mini robot ready...");
+
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonTicker, FALLING);
+  // Manual Debouncing capacitor expected! Else Circuit may malfunction!
 }
+
 void readPot()
 {
    SensVal[0] = analogRead(sensorPin0); 
    SensVal[1] = analogRead(sensorPin1); 
    SensVal[2] = analogRead(sensorPin2); 
 }
+
 void mapping(float state[])
 {
   state[0] = map(SensVal[0], 0, 512, 180, 0);
   state[1] = map(SensVal[1], 1024, 0, 270, 0);
   state[2] = map(SensVal[2], 1023, 0, 100, 270);
 }
+
 void Servowrite(float state[])
 {
   servo_0.write(state[0]);
   servo_1.write(state[1]);
   servo_2.write(state[2]);        
 }
+
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
-      buttonPushCounter++;
-      Serial.print("Number of button pushes: ");
-      Serial.println(buttonPushCounter);
-    }
-    delay(1000); 
+  if (buttonState) {
+    buttonPushCounter++;
+    Serial.print("Number of button pushes: ");
+    Serial.println(buttonPushCounter);
+    delay(1000);
+    buttonState = LOW;
   }
-  lastButtonState = buttonState;
   if (buttonPushCounter % 4 == 0) {
     Serial.println("Playing Steps....");
     digitalWrite(ledPin, HIGH);
